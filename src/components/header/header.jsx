a2684e30d4c54b37d9a934bccb59358df3de0512
Ana Sofia Serra <anasofiaserraluz@gmail.com>
2023-11-11 21:00:54 +0000

    test

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
 
     return (
     < div  className={style.header}>
-      <a href="#section1">Section 1</a>
+      <a href="#section1">Section 1 </a>
       <a href="#section2">Section 2</a>
       <a href="#section3">Section 3</a>
       <a href="#section4">Section 4</a>
